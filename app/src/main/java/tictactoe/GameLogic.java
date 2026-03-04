5beb5a06e7df3f9d57b5468b52eebb66eb53c07f
Henri Hollanti <henri.hollanti@hotmail.com>
2026-03-04 19:08:24 +0200

    feat(GameLogic): add checkWinner() implementation

--- app/src/main/java/tictactoe/GameLogic.java
+++ app/src/main/java/tictactoe/GameLogic.java
@@ -1,5 +1,22 @@
 package tictactoe;
 
 public final class GameLogic implements Logic{
+    private void checkWinner() {
+        int[][] winConditions = {
+            {0, 1, 2}, {3, 4, 5}, {6, 7, 8}, // Rows
+            {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, // Columns
+            {0, 4, 8}, {2, 4, 6}             // Diagonals
+        };
 
+        for (int[] pos : winConditions) {
+            String b1 = buttons[pos[0]].getText();
+            String b2 = buttons[pos[1]].getText();
+            String b3 = buttons[pos[2]].getText();
+
+            if (!b1.equals("") && b1.equals(b2) && b2.equals(b3)) {
+                javax.swing.JOptionPane.showMessageDialog(this, "Player " + b1 + " wins!");
+                resetBoard();
+            }
+        }
+    }
 }
